b6648163866dcc0073676aba08453833e38641bd
John Blackshear <swjaybee@gmail.com>
2019-07-13 00:55:49 -0500

    freinds.js

---
+++ app/friends.js
@@ -0,0 +1,65 @@
+var freindsArray = [
+    {
+        name: 'Troy Barnes',
+        photo:'https://cdn3.whatculture.com/images/2015/09/Community-Troy.jpg',
+        scores:[
+
+        ]
+    },
+    {
+        name: 'Abed Nadir',
+        photo: 'http://images4.fanpop.com/image/photos/20300000/Abed-abed-nadir-20355927-450-650.jpg',
+        scores:[
+
+        ]
+    },
+    {
+        name: 'Britta Perry',
+        photo: 'https://www.tvovermind.com/wp-content/uploads/2013/04/britta-perry.jpg',
+        scores:[
+
+        ]
+    },
+    {
+        name: 'Jeff Winger',
+        photo: 'http://mediad.publicbroadcasting.net/p/shared/npr/styles/placed_wide/nprshared/201805/393582368.jpg',
+        scores:[
+
+        ]
+    },
+    {
+        name: 'Annie Edison',
+        photo: 'https://vignette.wikia.nocookie.net/greendale/images/0/01/Annie.png/revision/latest?cb=20101008175611',
+        scores:[
+
+        ]
+    },
+    {
+        name: 'Shirley Bennett',
+        photo: 'https://cimg.tvgcdn.net/i/2014/09/30/afe134d9-80a3-4aad-9d4c-7ad94301d602/f2e1931cb7f0052473d8ab17499b353a/141001yvette-nicole-brown-community1.jpg',
+        scores:[
+
+        ]
+    },
+    {
+        name: 'Pierce Hawthorne',
+        photo: 'https://oyster.ignimgs.com/wordpress/stg.ign.com/2012/11/pierce-photo.jpg',
+        scores:[
+
+        ]
+    },
+    {
+        name: 'Senior Ben Chang',
+        photo: 'https://vignette.wikia.nocookie.net/community-sitcom/images/c/c9/S1-Ben_Chang.jpg/revision/latest/scale-to-width-down/300?cb=20090925220739',
+        scores:[
+
+        ]
+    },
+    {
+        name: 'Dean Craig Pelton',
+        photo: 'https://vignette.wikia.nocookie.net/community-sitcom/images/e/eb/Craig_Pelton_Season_One.jpg/revision/latest/scale-to-width-down/300?cb=20140227212347',
+        scores:[ 
+
+        ]
+    },
+]
