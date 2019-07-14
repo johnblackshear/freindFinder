ac806955b88c0e33c5d0547fe5f5955127801878
John Blackshear <swjaybee@gmail.com>
2019-07-14 15:25:14 -0500

    html/server

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,64 +2,66 @@ var freindsArray = [
     {
         name: 'Troy Barnes',
         photo:'https://cdn3.whatculture.com/images/2015/09/Community-Troy.jpg',
-        scores:[
+        scores:[ '5','2','1','1','1','3','3','1','5','3'
 
         ]
     },
     {
         name: 'Abed Nadir',
         photo: 'http://images4.fanpop.com/image/photos/20300000/Abed-abed-nadir-20355927-450-650.jpg',
-        scores:[
+        scores:['5','3','4','2','5','5','4','1','3','2'
 
         ]
     },
     {
         name: 'Britta Perry',
         photo: 'https://www.tvovermind.com/wp-content/uploads/2013/04/britta-perry.jpg',
-        scores:[
+        scores:['3','4','3','4','2','1','5','5','5','4'
 
         ]
     },
     {
         name: 'Jeff Winger',
         photo: 'http://mediad.publicbroadcasting.net/p/shared/npr/styles/placed_wide/nprshared/201805/393582368.jpg',
-        scores:[
+        scores:['1','5','5','2','3','5','5','1','1','3'
 
         ]
     },
     {
         name: 'Annie Edison',
         photo: 'https://vignette.wikia.nocookie.net/greendale/images/0/01/Annie.png/revision/latest?cb=20101008175611',
-        scores:[
+        scores:['2','4','4','2','3','1','3','3','4','2'
 
         ]
     },
     {
         name: 'Shirley Bennett',
         photo: 'https://cimg.tvgcdn.net/i/2014/09/30/afe134d9-80a3-4aad-9d4c-7ad94301d602/f2e1931cb7f0052473d8ab17499b353a/141001yvette-nicole-brown-community1.jpg',
-        scores:[
+        scores:['3','5','2','5','3','1','2','1','3','3'
 
         ]
     },
     {
         name: 'Pierce Hawthorne',
         photo: 'https://oyster.ignimgs.com/wordpress/stg.ign.com/2012/11/pierce-photo.jpg',
-        scores:[
+        scores:['5','3','3','5','2','3','5','5','2','5'
 
         ]
     },
     {
         name: 'Senior Ben Chang',
         photo: 'https://vignette.wikia.nocookie.net/community-sitcom/images/c/c9/S1-Ben_Chang.jpg/revision/latest/scale-to-width-down/300?cb=20090925220739',
-        scores:[
+        scores:['5','1','1','1','3','1','5','1','1','1'
 
         ]
     },
     {
         name: 'Dean Craig Pelton',
         photo: 'https://vignette.wikia.nocookie.net/community-sitcom/images/e/eb/Craig_Pelton_Season_One.jpg/revision/latest/scale-to-width-down/300?cb=20140227212347',
-        scores:[ 
+        scores:[ '5','3','4','1','2','1','1','1','1','5'
 
         ]
     },
 ]
+
+module.exports
