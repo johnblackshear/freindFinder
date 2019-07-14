b1fcb2022a3d70fef3b8fa1ffb148848cbb666b0
John Blackshear <swjaybee@gmail.com>
2019-07-14 18:31:14 -0500

    api

--- app/routing/apiRouting.js
+++ app/routing/apiRouting.js
@@ -1,8 +1,12 @@
 var matchFreind = require("../data/friends.js");
 
 module.exports =function(app){
-    app.get("api/freinds", function(req, res){
+    app.get("/api/freinds", function(req, res){
         res.json(matchFreind);
     });
     
-}
+    app.post("/api/freinds", function(req, res){
+        
+        
+    });
+};
