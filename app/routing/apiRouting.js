406bc7badba722ee417ab0362d9697674bdff580
John Blackshear <swjaybee@gmail.com>
2019-07-14 17:29:59 -0500

    routing

---
+++ app/routing/apiRouting.js
@@ -0,0 +1,8 @@
+var matchFreind = require("../data/friends.js");
+
+module.exports =function(app){
+    app.get("api/freinds", function(req, res){
+        res.json(matchFreind);
+    });
+    
+}
--- routing/apiRouting.js
+++
@@ -1,9 +0,0 @@
-var matchFreind = require("../app/data/friends.js");
-
-app.get("api/freinds", function(req, res){
-    res.json(matchFreind);
-});
-
-app.post("api/freinds"), function(req, res){
-
-}
