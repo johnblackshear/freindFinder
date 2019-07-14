78a2d33a64ea5a4fc1d03d80c03189133c70e44a
John Blackshear <swjaybee@gmail.com>
2019-07-14 14:08:52 -0500

    init

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -10,3 +10,6 @@ app.use(express.json());
 require("./routing/apiRouting")(app);
 require("./routing/htmlRoutes")(app);
 
+app.listen(PORT, function(){
+    console.log("App is listening on PORT:" + PORT);
+});
