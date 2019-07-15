6eec20133d21e1235b84d65ab6a0fea0801e82c0
John Blackshear <swjaybee@gmail.com>
2019-07-14 19:08:57 -0500

    init

--- app/routing/apiRouting.js
+++ app/routing/apiRouting.js
@@ -6,7 +6,33 @@ module.exports =function(app){
     });
     
     app.post("/api/freinds", function(req, res){
+        var newFreind = req.body;
+        var newFriendScore = req.body.scores;
+        var scoresArray = [];
+        var bestMatch = 0;
+
+        for(i = 0; i < matchFreind.length; i++){
+            var totalDifference = 0;
         
+
+        for(j = 0; j < newFriendScore.length; j++){
+            totalDifference += (Math.abs(parseInt(matchFreind[i].scores[j]) - parseInt(newFriendScore[j])));
+        }
+
+        scoresArray.push(totalDifference);
+
+    }
+
+    for(i = 0; i < scoresArray.length; i++){
+        if(scoresArray[i] <= scoresArray[bestMatch]){
+            bestMatch = i; 
+        }
+    }
+
+    var bestFreind = matchFreind[bestMatch];
+        res.json(bestFreind);
+
+    matchFreind.push(newFreind);
         
     });
 };
